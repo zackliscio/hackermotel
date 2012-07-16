0a3a68e8460cf1bcd8339c8a09b871ec7372a6ae
Zack Liscio <zackliscio@gmail.com>
2012-07-16 13:24:10 -0400

    new Rails app generated by Rails Apps Composer gem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hackermotel::Application.config.secret_token = 'ca5665121066759432a50950976e68b860a485b8e763feac29b1a9dbef370cca9762e65444c70e466912098b8aac90864f4284e6357a67726c29c47a0cbf17c4'
---
+++ db/migrate/20120716172331_add_optin_to_users.rb
@@ -0,0 +1,6 @@
+class AddOptinToUsers < ActiveRecord::Migration
+  def change
+    add_column :users, :opt_in, :boolean
+
+  end
+end
